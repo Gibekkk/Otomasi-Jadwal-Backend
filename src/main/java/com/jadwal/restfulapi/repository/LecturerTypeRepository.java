56802043b640ec76d815a5e72a201b46ffb68c40
Gibekkk <www.gilbertwinardy@gmail.com>
2026-08-07 18:23:27 +0800

    removal of lecturer type

--- src/main/java/com/jadwal/restfulapi/repository/LecturerTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jadwal.restfulapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jadwal.restfulapi.model.LecturerType;
-
-public interface LecturerTypeRepository extends JpaRepository<LecturerType, String> {
-}
