c640c9785756c1b2c394fdaa8bf282fd15d6bee6
Gibekkk <www.gilbertwinardy@gmail.com>
2026-08-14 12:36:09 +0800

    perbaikan controller timeline dan schedule

--- src/main/java/com/jadwal/restfulapi/controller/TimelineController.java
+++ src/main/java/com/jadwal/restfulapi/controller/TimelineController.java
@@ -3,38 +3,25 @@ package com.jadwal.restfulapi.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.jadwal.restfulapi.annotation.ErrorExample;
 import com.jadwal.restfulapi.annotation.NoAuth;
 import com.jadwal.restfulapi.annotation.SuccessExample;
 import com.jadwal.restfulapi.handler.StatusHandler;
-import com.jadwal.restfulapi.service.AuthService;
 import com.jadwal.restfulapi.service.FreeTableService;
-import com.jadwal.restfulapi.service.RoomService;
+import com.jadwal.restfulapi.service.ScheduleService;
 import com.jadwal.restfulapi.util.ErrorMessage;
 import com.jadwal.restfulapi.util.HTTPCode;
 
 import com.jadwal.restfulapi.model.FreeTable;
-import com.jadwal.restfulapi.model.Session;
-import com.jadwal.restfulapi.model.User;
-import com.jadwal.restfulapi.model.Room;
-import com.jadwal.restfulapi.dto.RoomDTO;
+import com.jadwal.restfulapi.model.Schedule;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -42,10 +29,7 @@ import java.util.List;
 public class TimelineController {
 
     @Autowired
-    private AuthService authService;
-
-    @Autowired
-    private RoomService roomService;
+    private ScheduleService scheduleService;
 
     @Autowired
     private FreeTableService freeTableService;
@@ -57,218 +41,21 @@ public class TimelineController {
 
     private Object data = "";
 
-    @SuccessExample(value = "[{\"id\":\"uuid\",\"name\":\"Jaringan Komputer\","
-            + "\"createdAt\":\"2026-01-01T00:00:00\",\"updatedAt\":\"2026-01-01T00:00:00\"}]")
-    @ErrorExample(code = "401", name = "session-invalid", message = "Authentication Failed")
-    @GetMapping
-    public ResponseEntity<Object> getRooms(HttpServletRequest request) {
-        String sessionToken = request.getHeader("Token");
+    @NoAuth
+    @SuccessExample(value = "[{\"id\":\"uuid\",\"timeStart\":\"7.30\",\"timeEnd\":\"8.20\"}]")
+    @ErrorExample(code = "404", name = "not-found", message = "Status Not Found")
+    @GetMapping("/schedules")
+    public ResponseEntity<Object> getShcedules() {
         HTTPCode httpCode = HTTPCode.OK;
         try {
-            Optional<Session> sessionOpt = authService.findSessionBySessionToken(sessionToken);
-            if (sessionOpt.isPresent()) {
-                List<Room> rooms = roomService.findAllRoom();
-                ArrayList<Map<String, Object>> roomList = new ArrayList<>();
-                for (Room room : rooms) {
-                    roomList.add(Map.ofEntries(
-                            Map.entry("id", room.getId()),
-                            Map.entry("name", room.getName()),
-                            Map.entry("createdAt", room.getCreatedAt()),
-                            Map.entry("updatedAt", room.getUpdatedAt())));
-                }
-                data = roomList;
-            } else {
-                httpCode = HTTPCode.UNAUTHORIZED;
-                data = new ErrorMessage(httpCode, "Authentication Failed");
+            ArrayList<Object> scheduleList = new ArrayList<Object>();
+            for (Schedule schedule : scheduleService.findAllSchedule()) {
+                scheduleList.add(Map.ofEntries(
+                        Map.entry("id", schedule.getId()),
+                        Map.entry("timeStart", schedule.getTimeStart()),
+                        Map.entry("timeEnd", schedule.getTimeEnd())));
             }
-        } catch (IllegalArgumentException e) {
-            httpCode = HTTPCode.BAD_REQUEST;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        } catch (Exception e) {
-            httpCode = HTTPCode.INTERNAL_SERVER_ERROR;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        }
-
-        return ResponseEntity
-                .status(httpCode.getStatus())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(data);
-    }
-
-    @SuccessExample(value = "{\"id\":\"uuid\",\"name\":\"Jaringan Komputer\","
-            + "\"createdAt\":\"2026-01-01T00:00:00\",\"updatedAt\":\"2026-01-01T00:00:00\"}")
-    @ErrorExample(code = "401", name = "session-invalid", message = "Authentication Failed")
-    @ErrorExample(code = "404", name = "not-found", message = "Room Not Found")
-    @GetMapping("/{roomId}")
-    public ResponseEntity<Object> getRoomById(HttpServletRequest request, @PathVariable String roomId) {
-        String sessionToken = request.getHeader("Token");
-        HTTPCode httpCode = HTTPCode.OK;
-        try {
-            Optional<Session> sessionOpt = authService.findSessionBySessionToken(sessionToken);
-            if (sessionOpt.isPresent()) {
-                Optional<Room> roomOpt = roomService.findRoomById(roomId);
-                if (roomOpt.isPresent()) {
-                    Room room = roomOpt.get();
-                    data = Map.ofEntries(
-                            Map.entry("id", room.getId()),
-                            Map.entry("name", room.getName()),
-                            Map.entry("createdAt", room.getCreatedAt()),
-                            Map.entry("updatedAt", room.getUpdatedAt()));
-                } else {
-                    httpCode = HTTPCode.NOT_FOUND;
-                    data = new ErrorMessage(httpCode, "Room Not Found");
-                }
-            } else {
-                httpCode = HTTPCode.UNAUTHORIZED;
-                data = new ErrorMessage(httpCode, "Authentication Failed");
-            }
-        } catch (IllegalArgumentException e) {
-            httpCode = HTTPCode.BAD_REQUEST;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        } catch (Exception e) {
-            httpCode = HTTPCode.INTERNAL_SERVER_ERROR;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        }
-
-        return ResponseEntity
-                .status(httpCode.getStatus())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(data);
-    }
-
-    @SuccessExample(value = "{\"message\":\"Room Deleted Successfully\"}")
-    @ErrorExample(code = "401", name = "session-invalid", message = "Authentication Failed")
-    @ErrorExample(code = "403", name = "access-denied", message = "Access Denied")
-    @ErrorExample(code = "404", name = "not-found", message = "Room Not Found")
-    @DeleteMapping("/{roomId}")
-    public ResponseEntity<Object> deleteRoom(HttpServletRequest request, @PathVariable String roomId) {
-        String sessionToken = request.getHeader("Token");
-        HTTPCode httpCode = HTTPCode.OK;
-        try {
-            Optional<Session> sessionOpt = authService.findSessionBySessionToken(sessionToken);
-            if (sessionOpt.isPresent()) {
-                Session session = sessionOpt.get();
-                User user = session.getUserId();
-                if (authService.isSuperAdmin(user) || authService.isPmAdmin(user)) {
-                    Optional<Room> roomOpt = roomService.findRoomById(roomId);
-                    if (roomOpt.isPresent()) {
-                        Room room = roomOpt.get();
-                        roomService.deleteRoom(room);
-                        data = Map.ofEntries(
-                                Map.entry("message", "Room Deleted Successfully"));
-                    } else {
-                        httpCode = HTTPCode.NOT_FOUND;
-                        data = new ErrorMessage(httpCode, "Room Not Found");
-                    }
-                } else {
-                    httpCode = HTTPCode.FORBIDDEN;
-                    data = new ErrorMessage(httpCode, "Access Denied");
-                }
-            } else {
-                httpCode = HTTPCode.UNAUTHORIZED;
-                data = new ErrorMessage(httpCode, "Authentication Failed");
-            }
-        } catch (IllegalArgumentException e) {
-            httpCode = HTTPCode.BAD_REQUEST;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        } catch (Exception e) {
-            httpCode = HTTPCode.INTERNAL_SERVER_ERROR;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        }
-
-        return ResponseEntity
-                .status(httpCode.getStatus())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(data);
-    }
-
-    @SuccessExample(value = "{\"id\":\"uuid\",\"name\":\"Jaringan Komputer\","
-            + "\"createdAt\":\"2026-01-01T00:00:00\",\"updatedAt\":\"2026-01-01T00:00:00\"}")
-    @ErrorExample(code = "401", name = "session-invalid", message = "Authentication Failed")
-    @ErrorExample(code = "403", name = "access-denied", message = "Access Denied")
-    @ErrorExample(code = "400", name = "invalid-body", message = "Name Cannot Be NULL")
-    @ErrorExample(code = "400", name = "capacity-invalid", message = "Capacity Must Be Greater Than 5")
-    @PostMapping
-    public ResponseEntity<Object> createRoom(HttpServletRequest request, @RequestBody RoomDTO roomDTO) {
-        String sessionToken = request.getHeader("Token");
-        HTTPCode httpCode = HTTPCode.OK;
-        try {
-            roomDTO.checkDTO();
-            Optional<Session> sessionOpt = authService.findSessionBySessionToken(sessionToken);
-            if (sessionOpt.isPresent()) {
-                Session session = sessionOpt.get();
-                User user = session.getUserId();
-                if (authService.isSuperAdmin(user) || authService.isPmAdmin(user)) {
-                    Room createdRoom = roomService.createRoom(roomDTO, user);
-                    data = Map.ofEntries(
-                            Map.entry("id", createdRoom.getId()),
-                            Map.entry("name", createdRoom.getName()),
-                            Map.entry("createdAt", createdRoom.getCreatedAt()),
-                            Map.entry("updatedAt", createdRoom.getUpdatedAt()));
-                } else {
-                    httpCode = HTTPCode.FORBIDDEN;
-                    data = new ErrorMessage(httpCode, "Access Denied");
-                }
-            } else {
-                httpCode = HTTPCode.UNAUTHORIZED;
-                data = new ErrorMessage(httpCode, "Authentication Failed");
-            }
-        } catch (IllegalArgumentException e) {
-            httpCode = HTTPCode.BAD_REQUEST;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        } catch (Exception e) {
-            httpCode = HTTPCode.INTERNAL_SERVER_ERROR;
-            data = new ErrorMessage(httpCode, e.getMessage());
-        }
-
-        return ResponseEntity
-                .status(httpCode.getStatus())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(data);
-    }
-
-    @SuccessExample(value = "{\"id\":\"uuid\",\"name\":\"Jaringan Komputer\","
-            + "\"createdAt\":\"2026-01-01T00:00:00\",\"updatedAt\":\"2026-01-02T00:00:00\"}")
-    @ErrorExample(code = "401", name = "session-invalid", message = "Authentication Failed")
-    @ErrorExample(code = "403", name = "access-denied", message = "Access Denied")
-    @ErrorExample(code = "404", name = "not-found", message = "Room Not Found")
-    @ErrorExample(code = "400", name = "invalid-body", message = "Name Cannot Be NULL")
-    @PutMapping("/{roomId}")
-    public ResponseEntity<Object> editRoom(HttpServletRequest request, @RequestBody RoomDTO roomDTO,
-            @PathVariable String roomId) {
-        String sessionToken = request.getHeader("Token");
-        HTTPCode httpCode = HTTPCode.OK;
-        try {
-            roomDTO.checkDTO();
-            Optional<Session> sessionOpt = authService.findSessionBySessionToken(sessionToken);
-            if (sessionOpt.isPresent()) {
-                Session session = sessionOpt.get();
-                User user = session.getUserId();
-                if (authService.isSuperAdmin(user) || authService.isPmAdmin(user)) {
-                    Optional<Room> editedRoomOpt = roomService.findRoomById(roomId);
-                    if (editedRoomOpt.isPresent()) {
-                        Room editedRoom = editedRoomOpt.get();
-                        editedRoom = roomService.editRoom(editedRoom, roomDTO, user);
-                        data = Map.ofEntries(
-                                Map.entry("id", editedRoom.getId()),
-                                Map.entry("name", editedRoom.getName()),
-                                Map.entry("createdAt", editedRoom.getCreatedAt()),
-                                Map.entry("updatedAt", editedRoom.getUpdatedAt()));
-                    } else {
-                        httpCode = HTTPCode.NOT_FOUND;
-                        data = new ErrorMessage(httpCode, "Room Not Found");
-                    }
-                } else {
-                    httpCode = HTTPCode.FORBIDDEN;
-                    data = new ErrorMessage(httpCode, "Access Denied");
-                }
-            } else {
-                httpCode = HTTPCode.UNAUTHORIZED;
-                data = new ErrorMessage(httpCode, "Authentication Failed");
-            }
-        } catch (IllegalArgumentException e) {
-            httpCode = HTTPCode.BAD_REQUEST;
-            data = new ErrorMessage(httpCode, e.getMessage());
+            data = scheduleList;
         } catch (Exception e) {
             httpCode = HTTPCode.INTERNAL_SERVER_ERROR;
             data = new ErrorMessage(httpCode, e.getMessage());
