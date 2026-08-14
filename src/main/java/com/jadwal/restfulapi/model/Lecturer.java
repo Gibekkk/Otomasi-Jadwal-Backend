e8294e834e50bfd4c468d24a7796baed8e93c899
Gilbert Winardy <www.gilbertwinardy@gmail.com>
2026-08-14 16:10:17 +0800

    Update Lecturer.java

--- src/main/java/com/jadwal/restfulapi/model/Lecturer.java
+++ src/main/java/com/jadwal/restfulapi/model/Lecturer.java
@@ -36,7 +36,7 @@ public class Lecturer {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @Column(name = "name", nullable = false, length = 255)
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
 
     @Column(name = "is_male", nullable = false)
