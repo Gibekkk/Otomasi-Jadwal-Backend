9de46b6649bc728a1db8be0261bf4bdc78f839d0
Gibekkk <www.gilbertwinardy@gmail.com>
2026-08-18 18:00:24 +0800

    fixed transactional

--- src/main/java/com/jadwal/restfulapi/repository/LecturerSpecializationRepository.java
+++ src/main/java/com/jadwal/restfulapi/repository/LecturerSpecializationRepository.java
@@ -3,6 +3,7 @@ package com.jadwal.restfulapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jadwal.restfulapi.model.LecturerSpecialization;
 import com.jadwal.restfulapi.model.Lecturer;
@@ -10,6 +11,10 @@ import com.jadwal.restfulapi.model.Specialization;
 
 public interface LecturerSpecializationRepository extends JpaRepository<LecturerSpecialization, String> {
     public List<LecturerSpecialization> findAllByLecturerId(Lecturer lecturerId);
+
+    @Transactional
     public void deleteAllByLecturerId(Lecturer lecturerId);
+
+    @Transactional
     public void deleteAllBySpecializationId(Specialization specializationId);
 }
