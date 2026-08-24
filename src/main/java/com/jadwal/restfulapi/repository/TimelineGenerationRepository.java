ac75f4bd7b3a400a9a831e30c48c9c6c2bf5084c
Gibekkk <www.gilbertwinardy@gmail.com>
2026-08-24 09:04:40 +0800

    clearing

--- src/main/java/com/jadwal/restfulapi/repository/TimelineGenerationRepository.java
+++ src/main/java/com/jadwal/restfulapi/repository/TimelineGenerationRepository.java
@@ -1,7 +1,5 @@
 package com.jadwal.restfulapi.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jadwal.restfulapi.model.TimelineGeneration;
