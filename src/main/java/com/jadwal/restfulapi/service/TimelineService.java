966bd0de85f74926c3b8a7a8766e9c29011f887d
Gibekkk <www.gilbertwinardy@gmail.com>
2026-08-26 18:24:24 +0800

    cleaning

--- src/main/java/com/jadwal/restfulapi/service/TimelineService.java
+++ src/main/java/com/jadwal/restfulapi/service/TimelineService.java
@@ -1,21 +1,15 @@
 package com.jadwal.restfulapi.service;
 
-import java.util.Optional;
-import java.util.UUID;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.jadwal.restfulapi.model.User;
-import com.jadwal.restfulapi.dto.GenerateDTO;
 import com.jadwal.restfulapi.model.Lecture;
 import com.jadwal.restfulapi.model.FreeTable;
-import com.jadwal.restfulapi.model.TimelineGeneration;
 import com.jadwal.restfulapi.repository.LectureRepository;
 import com.jadwal.restfulapi.repository.FreeTableRepository;
 import com.jadwal.restfulapi.repository.TimelineGenerationRepository;
-import com.jadwal.restfulapi.util.PasswordHasherMatcher;
 
 @Service
 public class TimelineService {
