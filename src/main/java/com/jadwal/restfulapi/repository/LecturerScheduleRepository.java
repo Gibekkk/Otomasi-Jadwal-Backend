db532d6d29b0fbe57e9709a01b9b49dde8df3a22
Gibekkk <www.gilbertwinardy@gmail.com>
2026-09-02 09:25:53 +0800

    cleaning

--- src/main/java/com/jadwal/restfulapi/repository/LecturerScheduleRepository.java
+++ src/main/java/com/jadwal/restfulapi/repository/LecturerScheduleRepository.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.jadwal.restfulapi.model.Lecturer;
 import com.jadwal.restfulapi.model.LecturerSchedule;
-import com.jadwal.restfulapi.model.Schedule;
 
 public interface LecturerScheduleRepository extends JpaRepository<LecturerSchedule, String> {
     public List<LecturerSchedule> findAllByLecturerId(Lecturer lecturerId);
